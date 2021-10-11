7ff362cf1b2f0da82653fc5f48a8e531083725c3
Max <mrymnik3d@gmail.com>
2021-10-11 18:24:20 +0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import QuestionBody from './components/QuestionBody';
+
+const API_url = "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [gameEnded, setGameEnded] = useState(false)
+
+  useEffect(() => {
+    axios.get(API_url).then(res => {
+      const dataQ = res.data;
+      setQuestions(dataQ.results);
+
+    });
+
+    // fetch(API_url)
+    //  .then(res => res.json())
+    //   .then((data) => {setQuestions(data.results)  }); ---Аналог axios---
+
+  }, []);
+  const maxQuestion = questions.length
+  const handleAnswer = (answer) => {
+    //перевірка відповіді
+    const newIndex = currentIndex + 1
+    setCurrentIndex(newIndex);
+    if (answer === questions[currentIndex].correct_answer) {
+      setScore(score+1);
+      alert("Correct");
+    }
+
+    
+    if(newIndex >=questions.length){
+      setGameEnded(true);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_title">React Quize App</div>
+
+      {gameEnded ?(<div><h1>Game End</h1> <span>Score:{score}</span></div> ) : (questions.length > 0 ? (
+        <div className="question_body">
+          <QuestionBody handleAnswer={handleAnswer} questions={questions[currentIndex]} currIndex = {currentIndex} maxQuestion={maxQuestion}/>
+        </div>
+      ) : (<h1>Loading..</h1>))
+      }
+
+
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/QuestionBody.jsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react'
+
+
+export default function QuestionBody({questions,handleAnswer, currIndex,maxQuestion}) {
+
+    const answers = [questions.correct_answer, ...questions.incorrect_answers].sort(()=>Math.random()-0.5);
+
+    console.log(answers);
+
+    return (
+        <Fragment>
+            <h3 className="category_title">{questions.category}</h3>
+            <h3 className="question_title" dangerouslySetInnerHTML={{ __html: questions.question }}></h3>
+            {<div className="number_of_question"><span>{currIndex+1}/<span>{maxQuestion}</span> </span></div>}
+            <div className="answer_list">
+
+                {answers.map((answer) =>{
+                    return  <button onClick={()=>handleAnswer(answer)} className={`${questions.correct_answer === answer ? 'item_c': 'item'} item ` } dangerouslySetInnerHTML={{ __html: answer }}></button>
+                })}
+
+               
+            </div>
+        </Fragment>
+    )
+}
